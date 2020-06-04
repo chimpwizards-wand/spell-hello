ce20c2c1d435e45be5f786371208d4768cbf95fe
Andres Fuentes <andres.f.fuentes@pwc.com>
2020-06-04 14:06:12 -0400

    First Draft for Hello command

---
+++ src/commands/Hello.ts
@@ -0,0 +1,29 @@
+import Debug from 'debug';
+import { Command } from  '@chimpwizards/wand'
+import { CommandDefinition, CommandParameter, CommandArgument } from '@chimpwizards/wand/commons/command/'
+const chalk = require('chalk');
+const debug = Debug("w:cli:hello");
+
+@CommandDefinition({ 
+    description: 'Say Hi to someone'
+})
+export class Hello extends Command  { 
+
+    @CommandArgument({ description: 'Body name', required: true})
+    name: string = '';
+
+    execute(yargs: any): void {
+        debug(`Hello ${this.name}`)
+        console.log(`Hello ${chalk.green(this.name)}!!!`)
+    }
+
+}
+
+export function register ():any {
+    debug(`Registering....`)
+    let command = new Hello();
+    debug(`INIT: ${JSON.stringify(Object.getOwnPropertyNames(command))}`)
+
+    return command.build()
+}
+
